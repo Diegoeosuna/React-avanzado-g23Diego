021748c12ce1b7a098c34684ab687a7b776f4b42
Diegoeosuna <diegoemilioosuna@hotmail.com>
2023-07-10 21:47:57 -0600

    Clase React Context

---
+++ 02-react-context/src/components/SongList/SongList.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import canciones from '@/assets/listaCanciones.json'
+import './songlist.css'
+
+
+const SongList = () => {
+    const [list, setList] = useState([])
+    const [loading, setLoading] = useState(true)
+
+useEffect(() => {
+    setTimeout(() => {
+        setList(canciones)
+        setLoading(false)
+    }, 2000)
+}, [])
+
+    return (
+    <section>
+        {loading
+        ? <h1>Cargando...</h1>
+        : list.map((song) => (
+            <div key={song.id} className='row-song'>
+                <h4>{song.title}</h4>
+                <p>{song.artist}</p>
+            </div>
+        ))}
+    </section>
+    )
+}
+
+export default SongList
